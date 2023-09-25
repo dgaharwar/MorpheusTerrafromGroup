225d6e4f3b9f6bec495d22c8bb0ea35aafe6ee95
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-09-25 18:22:40 +0100

    Create variables.tf

---
+++ Group/variables.tf
@@ -0,0 +1,9 @@
+variable "morpheus_url" {
+  description = "The URL of the Morpheus platform"
+  type        = string
+}
+
+variable "access_token" {
+  description = "The access token of the user account used to access the Morpheus platform"
+  type        = string
+}
